bcbb950bb8a07ce8857df7895826f7fc96f75c86
Luiz Alberto Silva Sales Marinho <betomsales@alu.ufc.br>
2025-09-30 11:10:43 -0300

    Add arquivo .py para estrutura de Streamlit

---
+++ layout_basico.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Meu Primeiro Layout no Streamlit")
+st.header("Seção de Introdução")
+st.subheader("Subseção")
+st.write("Aqui posso escrever texto livre, tabelas ou até dados.")
